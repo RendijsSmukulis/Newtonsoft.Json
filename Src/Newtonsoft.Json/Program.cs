a9f45127deabf010526b5263192d88d435eeab70
Rendijs Smukulis <rendijs.smukulis@gmail.com>
2018-06-10 20:02:34 +0300

    My own amazing work

---
+++ Src/Newtonsoft.Json/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Newtonsoft.Json
+
+{
+    class Wrapper
+    {
+        public int a { get; set; }
+
+        public string b { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonstr = "{\"a\":5, \"b\":\"str val\"}";
+            object z = null;
+
+            JsonSerializer jsonSerializer = JsonSerializer.CreateDefault(null);
+
+            // by default DeserializeObject should check for additional content
+            if (!jsonSerializer.IsCheckAdditionalContentSet())
+            {
+                jsonSerializer.CheckAdditionalContent = true;
+            }
+
+            using (JsonTextReader reader = new JsonTextReader(new StringReader(jsonstr)))
+            {
+                z = jsonSerializer.Deserialize(reader, typeof(Wrapper));
+            }
+
+            var deserred = JsonConvert.DeserializeObject<Wrapper>(jsonstr);
+
+            Debugger.Break();
+        }
+    }
+}
